27a6b0e85aed072c5c1c433c779380e11bcdb027
unknown <kikoolol@.(none)>
2011-05-25 17:49:15 +0200

    Scheduler and pre download options set

--- src/eu/cyanogen/downloader/EditPreferences.java
+++ src/eu/cyanogen/downloader/EditPreferences.java
@@ -3,32 +3,38 @@ package eu.cyanogen.downloader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class EditPreferences extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		CharSequence[] entries = new CharSequence[40];
-		CharSequence[] values = new CharSequence[40];
-		int i=0;
-		ArrayList<HashMap<String,String>> phonesType = ParseData.getInstance().retrievePhonesList();
-		for (HashMap<String, String> hashMap : phonesType) {
-			entries[i]=hashMap.get("name");
-			values[i]=hashMap.get("link");
-			i++;
-		}
-		try{
-			ListPreference customPref = (ListPreference) findPreference("phoneType");
-			customPref.setEntries(entries);
-			customPref.setEntryValues(values);
-		}catch(Exception e){
-			ParseData.getInstance().displayToast("EditPrefError : "+e.toString());
-		}
+
+// This doesn't work need to parse data by hand ....
+//		ArrayList<CharSequence> entries = new ArrayList<CharSequence>();
+//		ArrayList<CharSequence> values = new ArrayList<CharSequence>();
+//		ArrayList<HashMap<String,String>> phonesType = ParseData.getInstance().retrievePhonesList();
+//		for (HashMap<String, String> hashMap : phonesType) {
+//			entries.add(hashMap.get("name"));
+//			values.add(hashMap.get("link"));
+//		}
+//		try{
+//			ListPreference customPref = (ListPreference)findPreference("phoneType");
+//			if(customPref!=null)
+//			{
+//				customPref.setEntries(entries.toArray(new CharSequence[entries.size()]));
+//				customPref.setEntryValues(values.toArray(new CharSequence[values.size()]));
+//			}
+//		}catch(Exception e){
+//			Log.v("Exception EditPreferences",e.toString());
+//		}
 		addPreferencesFromResource(R.xml.preferences);
 	}
+
 }
--- src/eu/cyanogen/downloader/downloader.java
+++ src/eu/cyanogen/downloader/downloader.java
@@ -1,6 +1,18 @@
 package eu.cyanogen.downloader;
 
+import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -8,6 +20,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -16,17 +31,55 @@ public class downloader extends Activity implements OnSharedPreferenceChangeList
 	
 	ListView displayData;
 	private static String URL="http://download.cyanogenmod.com";
+	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	private String url;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         displayData=(ListView)findViewById(R.id.displayData);
-
+        /*Set click*/
+        displayData.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+        	@SuppressWarnings("unchecked")
+         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+        		final HashMap<String, String> map = (HashMap<String, String>) displayData.getItemAtPosition(position);
+        		AlertDialog.Builder adb = new AlertDialog.Builder(downloader.this);
+        		adb.setTitle("Download this version ?");
+        		adb.setMessage("Your choice : "+map.get("name")+"\n"+map.get("size")+"\n"+map.get("date"));
+        		adb.setPositiveButton("Ok", new OnClickListener() {@Override public void onClick(DialogInterface arg0, int arg1) {downloader(map.get("link"));}});
+        		adb.setNegativeButton("Cancel", null);
+        		adb.show();
+        	}
+         });
+        
+        /*Get preferences and display data*/
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
-        url=prefs.getString("downloadUrl",URL);
+        url=prefs.getString("downloadUrl",URL)+prefs.getString("phoneType","/");
         retrieveInformation();
+        startChecker();
+    }
+    /**
+     * By default check update every hours
+     */
+    private void startChecker()
+    {
+    	scheduler.schedule(new Runnable(){public void run(){checker();}},60*60,TimeUnit.SECONDS);
+    }
+    private void checker()
+    {
+    	/*Notification*/
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new Notification(R.drawable.icon, "A new version of cyanogen is available", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, downloader.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(getApplicationContext(), "Cyanogen Update", "Version VERSION is now out for PHONE and NIGHTLY", contentIntent);
+        mNotificationManager.notify(1, notification);
+    }
+    private void downloader(String url)
+    {
+    	String urlD = PreferenceManager.getDefaultSharedPreferences(this).getString("downloadUrl",URL)+url;
     }
     private void retrieveInformation()
     {
@@ -60,6 +113,10 @@ public class downloader extends Activity implements OnSharedPreferenceChangeList
 		if(arg1.equals("downloadUrl"))
 		{
 			url=arg0.getString("downloadUrl",URL);
+		}
+		if(arg1.equals("phoneType"))
+		{
+			url=arg0.getString("downloadUrl",URL)+arg0.getString("phoneType","/");
 		}
 	}
 }
